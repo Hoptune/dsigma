60416877b813765295c5fb3dc713df78f1ca925a
Zhiwei Shao <magicianshao@outlook.com>
2025-09-12 21:54:01 -0400

    allow p(z) calculation in sigma_crit

--- dsigma/precompute_engine.pyx
+++ dsigma/precompute_engine.pyx
@@ -162,7 +162,7 @@ def precompute_engine(
     cdef double w_ls, sigma_crit, inv_sigma_crit
     cdef double max_pixrad = 1.05 * hp.pixel_resolution.to(u.deg).value
     cdef double inf = float('inf'), summand
-    cdef double[::1] pz_sel
+    cdef double* pz_ptr
 
     if progress_bar:
         pbar = tqdm(total=len(u_pix_l))
@@ -239,9 +239,9 @@ def precompute_engine(
                         sigma_crit = sigma_crit_eff[
                             i_l * n_z_bins + z_bin[i_s]]
                     elif has_pz:
-                        pz_sel = pz[i_s * n_z_mids:(i_s + 1) * n_z_mids]
+                        pz_ptr = &pz[i_s * n_z_mids]
                         inv_sigma_crit = get_avg_inv_sigma_crit(
-                            z_mids, d_com_zmids, pz_sel,
+                            z_mids, d_com_zmids, pz_ptr,
                             z_l[i_l], d_com_l[i_l])
                         if inv_sigma_crit == 0:
                             sigma_crit = inf
